525f92a9070b4fe3c95a790eb18758f3489cb977
Olivier <olivier@ogasphere.com>
2021-10-15 18:01:14 +0200

    crud admin post et category

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,31 +2,49 @@
 
 namespace App\Controller\Admin;
 
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use App\Entity\Post;
+use App\Entity\User;
+use App\Entity\Category;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
+;
 class DashboardController extends AbstractDashboardController
 {
     /**
      * @Route("/admin", name="admin")
+     * 
      */
     public function index(): Response
     {
-        return parent::index();
+        return $this->render('bundles/EasyAdminBundle/welcome.html.twig');
+      //  return parent::index();
     }
-
+    
+    /**
+     * configureDashboard
+     *
+     * @return Dashboard
+     */
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Easyadmin Test Blog');
+            ->setTitle('Administration d\'OBlog');
     }
-
+    
+    /**
+     * configureMenuItems
+     *
+     * @return iterable
+     */
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('Category', 'fas fa-list', Category::class);
+        yield MenuItem::linkToCrud('Post', 'fas fa-newspaper', Post::class);
+       // yield MenuItem::linkToCrud('User', 'fas fa-user', User::class);
     }
 }
--- src/Controller/Admin/PostCrudController.php
+++ src/Controller/Admin/PostCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Post;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class PostCrudController extends AbstractCrudController
@@ -12,14 +15,16 @@ class PostCrudController extends AbstractCrudController
         return Post::class;
     }
 
-    /*
+  
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+         //   IdField::new('id'),
             TextField::new('title'),
-            TextEditorField::new('description'),
+            SlugField::new('slug')->setTargetFieldName('slug'),
+            TextEditorField::new('content'),
+           // TextField::new('category'),
         ];
     }
-    */
+ 
 }
